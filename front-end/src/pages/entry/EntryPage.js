be13f300bc71fd7f574a4acc9e9388a0182b0547
Aftermath001 <alvinasiachi@gmail.com>
2024-05-23 09:24:28 +0300

    Password Reset page

--- front-end/src/pages/entry/EntryPage.js
+++ front-end/src/pages/entry/EntryPage.js
@@ -1,12 +1,76 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './entryStyle.css'
 import Login from '../../components/login/Login'
+import PasswordReset from '../../components/passwordreset/PasswordReset'
 
-const EntryPage = () => {
+
+
+const Entrypage = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [formload, setFormLoad] =useState("login")
+
+    const handleOnChange = (e) =>{
+        const {name, value} = e.target;
+        switch(name){
+            case 'email':
+                setEmail(value)
+                break
+            
+            case 'password':
+                setPassword(value)
+                break
+
+            default:
+                break
+        }
+
+        // console.log (name, value)
+
+    }
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        if (!email || !password){
+           return alert("Fill User Credentials");
+        }
+        console.log(email,password)
+    }
+    const handleResetOnSubmit = (e) => {
+        e.preventDefault();
+
+        if (!email){
+           return alert("Enter email");
+        }
+        console.log(email)
+    }
+    const switchform = formtype =>{
+        setFormLoad(formtype)
+    }
   return (
     <div className='entry-page bg-black'>
-        <Login/>
+        {formload === 'login' && 
+         <Login
+         handleOnSubmit={handleOnSubmit} 
+         handleOnChange={handleOnChange}
+         switchform ={switchform}
+         email={email}
+         password={password}
+         />}
+
+        {formload === 'reset' &&
+        <PasswordReset 
+        handleResetOnSubmit={handleResetOnSubmit} 
+        handleOnChange={handleOnChange}
+        switchform ={switchform}
+        email={email}
+       
+       />}
+        
     </div>
   )
 }
 
-export default EntryPage
+export default Entrypage
