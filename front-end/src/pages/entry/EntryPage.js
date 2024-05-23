8a4af459340efa0d96d285e2f46551fa45dcd689
Aftermath001 <alvinasiachi@gmail.com>
2024-05-23 09:21:16 +0300

    Login Page

--- front-end/src/pages/entry/EntryPage.js
+++ front-end/src/pages/entry/EntryPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from '../../components/login/Login'
 
 const EntryPage = () => {
   return (
     <div className='entry-page bg-black'>
-        
+        <Login/>
     </div>
   )
 }
