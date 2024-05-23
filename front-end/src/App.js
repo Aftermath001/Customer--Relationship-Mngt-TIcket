8c64378436d26445bc9171364836c398741fe2be
Aftermath001 <alvinasiachi@gmail.com>
2024-05-23 12:44:58 +0300

    Add New Ticket

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
-// import AddTicket from './pages/new-ticket/AddTicket';
+import AddTicket from './pages/new-ticket/AddTicket';
 // import TicketListing from './pages/ticket-listing/TicketListing';
 // import Ticket from './pages/ticket/Ticket';
 import Dashboard from './pages/dashboard/Dashboard';
@@ -15,7 +15,7 @@ function App() {
         <Route path='/' element={<Layout/>}>
             <Route index element={<Entrypage/>}/>
             <Route path='dashboard' element={<Dashboard/>}/>
-            {/* <Route path='addticket' element={<AddTicket />}/> */}
+            <Route path='addticket' element={<AddTicket />}/>
             {/* <Route path='tickets' element={<TicketListing />}/> */}
             {/* <Route path='ticket/:id' element={<Ticket />}/> */}
           </Route>
