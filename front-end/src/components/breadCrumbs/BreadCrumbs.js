c8755ca3f034c32e52fb301f25a4fe98a1f72f3b
Aftermath001 <alvinasiachi@gmail.com>
2024-05-23 12:37:49 +0300

    BreadCrumbs

--- front-end/src/components/breadCrumbs/BreadCrumbs.js
+++ front-end/src/components/breadCrumbs/BreadCrumbs.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
+// BreadCrumbs.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './breadcrumbs.css'; // Import custom CSS
 
-
-const BreadCrumbs = ({page}) => {
+const BreadCrumbs = ({ page }) => {
   return (
-    <Breadcrumb>
-      <Breadcrumb.Item href="/dashboard">Home</Breadcrumb.Item>
-      <Breadcrumb.Item active>{page}</Breadcrumb.Item>
-    </Breadcrumb>
-  )
-}
+    <div className="breadcrumbs">
+      <Link to="/" className="breadcrumb-link">Home</Link> / <span className="breadcrumb-current">{page}</span>
+    </div>
+  );
+};
 
-export default BreadCrumbs
+export default BreadCrumbs;
